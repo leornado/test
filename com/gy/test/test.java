8c8101df16f2ad25989ad5c918eb6fa39fb87acc
leornado <leornado@126.com>
2018-04-14 18:14:20 +0800

    bla bla bla

---
+++ com/gy/test/test.java
@@ -0,0 +1,9 @@
+package com.gy.test;
+
+public class Test {
+
+	public static void main(String[]args) {
+		System.err.println("Hello World!");
+	}
+
+}
---
+++ js/test.js
@@ -0,0 +1,7 @@
+var a = 123;
+var b;
+if (a === b)
+	console.log('a equals b');
+var x = 123, y = '123';
+if (x === y) 
+	console.log('x equals y');
